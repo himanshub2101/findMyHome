d0aa22d3122a1c20f24fa3a180bb9994464c3c0c
Meghna Gupta <108588561+meghnagupta-2002@users.noreply.github.com>
2023-11-22 16:34:27 +0530

    email.controller.js

--- api/controller/email.controller.js
+++ api/controller/email.controller.js
@@ -1,26 +1,35 @@
 import nodemailer from "nodemailer"
 
-export default function sendEmail(email,password)
-{
-    var transporter=nodemailer.createTransport({
-        serivce:"gmail",
-        auth:{
-            user:"akhil.hammoq@gmail.com",
-            pass:"yoyohoneysingh"
+export default function sendEmail(email, password,otp) {
+    var transporter = nodemailer.createTransport({
+        serivce: "gmail",
+        auth: {
+            user: "akhil.hammoq@gmail.com",
+            pass: "yoyohoneysingh"
         }
     })
 
-    var mailOptions={
-        from:"akhil.hammoq@gmail.com",
-        to:email,
-        subject:"verification email",
-        html:" <h1>Welcome to LRIT<h1/><p>You have succesfully registered on our application, Your login credentials are attached below<p/><h2>Login Detials<h2/><h3>User Id : "+email+"<h3/><h3>Password : "+password+"<h3/><h2>Click on the link below to verify your account<h2/>http://localhost:3000/verifyuser/"+email
-    }
+    var mailOptions = {
+        from: "akhil.hammoq@gmail.com",
+        to: email,
+        subject: "Verification email",
+        html: `
+        <h1>Welcome to findMyHome</h1>
+        <p>You have successfully registered on our application. Your login credentials are attached below.</p>
+        <h2>Login Details</h2>
+        <h3>User Id: ${email}</h3>
+        <h3>Password: ${password}</h3>
+        <h3>OTP: ${otp}</h3>
+        <h2>Click on the link below to verify your account</h2>
+        <a href="http://localhost:3000/verifyuser/${email}">Verification Link</a>
+    `
+    };
 
-    transporter.sendMail(mailOptions,function(err,info){
-        if(err)
-        console.log(err.message)
+
+    transporter.sendMail(mailOptions, function (err, info) {
+        if (err)
+            console.log(err.message)
         else
-        console.log("email sent:"+info.response)
+            console.log("email sent:" + info.response)
     });
 }
