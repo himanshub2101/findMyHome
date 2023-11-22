56ce28dab73ef1ea221bd1937a7a87099111ce5b
Meghna Gupta <108588561+meghnagupta-2002@users.noreply.github.com>
2023-11-22 16:33:35 +0530

    userController.js

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -8,10 +8,10 @@ import sendEmail1 from "./email.forgotpassword.js"
 import sendEmail from "./email.controller.js"
 import url from "url"
 
-const createToken=async(id)=>{
+const createToken = async (id) => {
     try {
-        const token=jwt.sign({_id:id},config.secrete_Key)
-        
+        const token = jwt.sign({ _id: id }, config.secrete_Key)
+
         return token;
     } catch (error) {
         res.status(400).json(error.message)
@@ -32,28 +32,32 @@ const securePassword = async (password) => {
 
 export const save = async (req, res) => {
     try {
-        
-        
-            var check = await userSchemaModel.findOne({ email: req.body.email })
-            if (check)
-                res.status(400).send("email allready exist")
-            else {
-                var user = await userSchemaModel.find().sort({ "_id": -1 })
-                var _id = user.length == 0 ? 1 : user[0]._id + 1
-                var iconname = req.file.filename
-                var spassword = await securePassword(req.body.password)
-                console.log(spassword)
-                var userDetails = { ...req.body, "_id": _id, "iconname": iconname, "token": "", "role": "user", "status": true }
-                userDetails.password = spassword;
-                
-                // res.send("working")
-                // sendEmail(userDetails.email,userDetails.password);
-    
-                await userSchemaModel.create(userDetails)
-                res.status(200).json({ status: "Record saved succesfully", "userdetails": userDetails })
+
+
+        var check = await userSchemaModel.findOne({ email: req.body.email })
+        if (check)
+            res.status(400).send("email allready exist")
+        else {
+            var user = await userSchemaModel.find().sort({ "_id": -1 })
+            var _id = user.length == 0 ? 1 : user[0]._id + 1
+            var iconname = req.file.filename
+            var spassword = await securePassword(req.body.password)
+            console.log(spassword)
+            var userDetails = { ...req.body, "_id": _id, "iconname": iconname, "token": "", "role": "user", "status": true }
+            userDetails.password = spassword;
+            let key = rs.generate({
+                length: 6,
+                charset: 'numeric' 
+            });
             
+            // res.send("working")
+            sendEmail(userDetails.email,userDetails.password,key);
+
+            await userSchemaModel.create(userDetails)
+            res.status(200).json({ status: "Record saved succesfully", otp:key, userdetails:userDetails })
+
         }
-        
+
 
 
     } catch (error) {
@@ -115,28 +119,27 @@ export var updateUser = async (req, res, next) => {
 export var login = async (req, res) => {
     try {
         const email = req.body.email
-        const password=req.body.password
+        const password = req.body.password
 
-    var user = await userSchemaModel.findOne({email:email})
-    
-    if (user)
-        {var passwordMatch=await bcryptjs.compare(password,user.password)
-            
+        var user = await userSchemaModel.findOne({ email: email })
+
+        if (user) {
+            var passwordMatch = await bcryptjs.compare(password, user.password)
+
             console.log(passwordMatch)
-        if(passwordMatch)
-        {
-            const token=await createToken(user._id)
-            user.token=token;
+            if (passwordMatch) {
+                const token = await createToken(user._id)
+                user.token = token;
 
-            res.status(200).json({status:true,userDetails:user})
+                res.status(200).json({ status: true, userDetails: user })
+            }
+            else {
+                res.status(200).json({ status: false, msg: "Invalid Password" })
+            }
         }
-        else{
-            res.status(200).json({status:false,msg:"Invalid Password"})
-        }
-    }
-        
-    else
-        res.status(200).json({status:false,msg:"Invalid credentials"})
+
+        else
+            res.status(200).json({ status: false, msg: "Invalid credentials" })
     } catch (error) {
         res.status(400).json(error.message)
     }
@@ -158,7 +161,7 @@ export const resetPassword = async (req, res) => {
             const password = req.query.password;
             let newPassword = await securePassword(password)
 
-            await userSchemaModel.updateOne({ _id: user._id }, { $set: { password: newPassword,token:"" } })
+            await userSchemaModel.updateOne({ _id: user._id }, { $set: { password: newPassword, token: "" } })
 
             res.status(200).send({ sucess: true, message: "password updated sucessfully" })
         }
