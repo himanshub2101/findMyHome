d437154ab773f36ec3b53dc5dd3a4a88d0604f27
Meghna Gupta <108588561+meghnagupta-2002@users.noreply.github.com>
2023-12-15 01:04:31 +0530

    Delete backend directory

--- backend/config/config.js
+++
@@ -1,7 +0,0 @@
-const config={
-    secrete_Key:"akhilakhil123654789",
-    email:"akhilrockpartycomcom@gmail.com",
-    password:"gjvc gurb ovfl wgcc"
-}
-
-export default config;
--- backend/registration/models/connection.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose"
-const url="mongodb://127.0.0.1:27017/FindMyHome"
-mongoose.connect(url)
-console.log("mongodb connected successfully")
