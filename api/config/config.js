2df6b839c746618952f702b646028a389b36e490
Meghna Gupta <108588561+meghnagupta-2002@users.noreply.github.com>
2023-12-15 01:02:16 +0530

    Delete api directory

--- api/config/config.js
+++
@@ -1,5 +0,0 @@
-const config={
-    secrete_Key:"akhilakhil123654789"
-}
-
-export default config;
--- api/models/connection.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose"
-const url="mongodb://127.0.0.1:27017/lrit"
-mongoose.connect(url);
-console.log("mongodb connected succesfully")
